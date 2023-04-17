c6336bbe41077ce8d446c0aa09ac4108ed318d9a
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-04-17 17:35:06 +0200

    reorganize projet folder structure app

---
+++ app/src/main/app/ui/home/generator_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:underscore_48484645478084414891891/underscore_48484645478084414891891.dart';
+import '../../ods_application.dart';
+
+class GeneratorPage extends StatelessWidget {
+  var calculator = Calculator();
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 3,
+            child: HistoryListView(),
+          ),
+          SizedBox(height: 10),
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  calculator.addValue(12);
+                  print(calculator.get());
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+              SizedBox(width: 10),
+              TotoWidget()
+            ],
+          ),
+          Spacer(flex: 2),
+        ],
+      ),
+    );
+  }
+}
