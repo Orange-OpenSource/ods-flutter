10c1f0d5211e0a88b194615b62c3f41af051b514
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-04-17 17:47:06 +0200

    fix test/widget_test

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter2/main.dart';
+import 'package:flutter2/ods_application.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
