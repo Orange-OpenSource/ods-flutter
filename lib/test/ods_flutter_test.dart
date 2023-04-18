414c17e26e584e9448a5ae0023ab1417860cee4e
Julien Déramond <julien.deramond@orange.com>
2023-04-18 14:08:25 +0200

    Lib: fix unused variables in `ods_flutter_test.dart`

--- lib/test/ods_flutter_test.dart
+++ lib/test/ods_flutter_test.dart
@@ -1,13 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:ods_flutter/ods_flutter.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    // expect(calculator.addValue(2), 3);
-    // expect(calculator.addValue(-7), -6);
-    expect(true, true);
-    // expect(calculator.addValue(0), 1);
+  test('check the theme existence when loading ods_flutter lib', () {
+    expect(totoThemeData, isNot(null));
   });
 }
