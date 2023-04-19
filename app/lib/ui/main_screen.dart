8c17e03c63529e65418135bf8710fe8df00cc52b
Pauline Auvray <pauline.auvray@orange.com>
2023-04-19 11:43:55 +0200

    Create a configuration for main screen and use it

--- app/lib/ui/main_screen.dart
+++ app/lib/ui/main_screen.dart
@@ -5,35 +5,26 @@ import 'components/components_screen.dart';
 import 'guidelines/guidelines_screen.dart';
 import 'modules/modules_screen.dart';
 
+const int mobileUiMaxScreenWidth = 500;
+const int extendedNavigationRailMinScreenWidth = 600;
+
 class MainScreen extends StatefulWidget {
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
-  var selectedIndex = 0;
+  var _selectedIndex = 0;
+  var _bottomNavigationBarItems = _MainScreenConfig.mainMenuItems
+      .map((e) => BottomNavigationBarItem(icon: e.icon, label: e.label))
+      .toList();
+  var _navigationRailDestinations = _MainScreenConfig.mainMenuItems
+      .map((e) => NavigationRailDestination(icon: e.icon, label: Text(e.label)))
+      .toList();
 
   @override
   Widget build(BuildContext context) {
     var colorScheme = Theme.of(context).colorScheme;
-
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GuidelinesScreen();
-        break;
-      case 1:
-        page = ComponentsScreen();
-        break;
-      case 2:
-        page = ModulesScreen();
-        break;
-      case 3:
-        page = AboutScreen();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
 
     // The container for the current page, with its background color
     // and subtle switching animation.
@@ -41,68 +32,39 @@ class _MainScreenState extends State<MainScreen> {
       color: colorScheme.surfaceVariant,
       child: AnimatedSwitcher(
         duration: Duration(milliseconds: 200),
-        child: page,
+        child: _MainScreenConfig.mainMenuItems[_selectedIndex].screen,
       ),
     );
 
     return Scaffold(
-        bottomNavigationBar: MediaQuery.of(context).size.width < 500
-            ? BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                items: [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.square),
-                    label: 'Guidelines',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.input),
-                    label: 'Components',
-                  ),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.check_box),
-                      label: 'Modules'
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.favorite),
-                    label: 'About',
-                  ),
-                ],
-                currentIndex: selectedIndex,
-                onTap: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-              )
-            : null,
+        appBar: AppBar(
+          title: Text(_MainScreenConfig.mainMenuItems[_selectedIndex].label),
+        ),
+        bottomNavigationBar:
+            MediaQuery.of(context).size.width < mobileUiMaxScreenWidth
+                ? BottomNavigationBar(
+                    type: BottomNavigationBarType.fixed,
+                    items: _bottomNavigationBarItems,
+                    currentIndex: _selectedIndex,
+                    onTap: (value) {
+                      setState(() {
+                        _selectedIndex = value;
+                      });
+                    },
+                  )
+                : null,
         body: Row(
           mainAxisSize: MainAxisSize.max,
           children: [
-            if (MediaQuery.of(context).size.width >= 500)
+            if (MediaQuery.of(context).size.width >= mobileUiMaxScreenWidth)
               NavigationRail(
-                extended: MediaQuery.of(context).size.width >= 600,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.square),
-                    label: Text('Guidelines'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.input),
-                    label: Text('Components'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.check_box),
-                    label: Text('Modules'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('About'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
+                extended: MediaQuery.of(context).size.width >=
+                    extendedNavigationRailMinScreenWidth,
+                destinations: _navigationRailDestinations,
+                selectedIndex: _selectedIndex,
                 onDestinationSelected: (value) {
                   setState(() {
-                    selectedIndex = value;
+                    _selectedIndex = value;
                   });
                 },
               ),
@@ -110,4 +72,38 @@ class _MainScreenState extends State<MainScreen> {
           ],
         ));
   }
+}
+
+class _MainScreenConfig {
+  static List<_MainMenuItem> mainMenuItems = [
+    _MainMenuItem(
+        icon: Icon(Icons.square),
+        label: 'Guidelines',
+        screen: GuidelinesScreen()),
+    _MainMenuItem(
+        icon: Icon(Icons.input),
+        label: 'Components',
+        screen: ComponentsScreen()),
+    _MainMenuItem(
+        icon: Icon(Icons.check_box), label: 'Modules', screen: ModulesScreen()),
+    _MainMenuItem(
+        icon: Icon(Icons.favorite), label: 'About', screen: AboutScreen()),
+  ];
+}
+
+class _MainMenuItem {
+  const _MainMenuItem({
+    required this.icon,
+    required this.label,
+    required this.screen,
+  });
+
+  /// The icon of the menu item.
+  final Widget icon;
+
+  /// The text label for this menu item.
+  final String label;
+
+  /// The screen launched on menu item tap.
+  final Widget screen;
 }
