31ac65be090a0d421c8a34f97fb48e0c0adcb8ad
Julien Déramond <julien.deramond@orange.com>
2023-04-20 11:27:09 +0200

    Lib: Drop temp `Calculator` and `TotoWidget` (#96)

--- app/lib/ui/modules/modules_screen.dart
+++ app/lib/ui/modules/modules_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ods_flutter/ods_flutter.dart';
 
 class ModulesScreen extends StatelessWidget {
-  var calculator = Calculator();
-
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
