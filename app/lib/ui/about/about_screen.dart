3513c4044bc76a957e959c1e8e92abb0dd217dc0
Pauline Auvray <pauline.auvray@orange.com>
2023-04-21 15:43:38 +0200

    117 - About page update (#120)
    * Add the illustration image in the assets and in the page
    
    * Updated the TextTheme to allow all available M3 system tokens, and commented the file to show M2 and iOS correspondances
    
    * Added the build version number, and corrected a theme ',' error
    
    * Fix text display and remove useless comment
    
    ---------
    
    Co-authored-by: Benoit SUZANNE <benoit.suzanne@orange.com>

--- app/lib/ui/about/about_screen.dart
+++ app/lib/ui/about/about_screen.dart
@@ -1,11 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/ods_flutter_localizations.dart';
-import 'package:ods_flutter/ods_flutter.dart';
+import 'package:ods_flutter/guidelines/spacings.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class AboutScreen extends StatelessWidget {
+class AboutScreen extends StatefulWidget {
+  const AboutScreen({Key? key}) : super(key: key);
+
+  @override
+  _AboutScreenState createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  // TODO: correct version package insertion
+  // Package Version instantiation
+  PackageInfo _packageInfo = PackageInfo(
+      appName: 'Unknown',
+      packageName: 'Unknown',
+      version: 'Unknown',
+      buildNumber: 'Unknown');
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final PackageInfo info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Text(AppLocalizations.of(context)!.aboutPageAppDescription));
+        child: Column(
+      children: [
+        Image(image: AssetImage('assets/il_about.png')),
+        Padding(
+          padding: const EdgeInsets.all(spacingM),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(AppLocalizations.of(context)!.appTitle,
+                  style: Theme.of(context).textTheme.headlineLarge),
+              SizedBox(height: spacingXs),
+              Text(
+                  'Version ${_packageInfo.version} (build ${_packageInfo.buildNumber})',
+                  style: Theme.of(context).textTheme.bodySmall),
+              SizedBox(height: spacingXs),
+              Text(AppLocalizations.of(context)!.aboutPageAppDescription,
+                  style: Theme.of(context).textTheme.bodyMedium),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
