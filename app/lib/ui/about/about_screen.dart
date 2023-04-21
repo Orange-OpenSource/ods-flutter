45dc4c7b0faba4bdbec1bebaead7f4cf5ea1b0c2
Benoit SUZANNE <benoit.suzanne@orange.com>
2023-04-21 11:37:18 +0200

    Add the illustration image in the assets and in the page

--- app/lib/ui/about/about_screen.dart
+++ app/lib/ui/about/about_screen.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/ods_flutter_localizations.dart';
-import 'package:ods_flutter/ods_flutter.dart';
 
 class AboutScreen extends StatelessWidget {
+  // TODO: correct version package insertion
+  // Package Version instantiation
+  // PackageInfo _packageInfo = PackageInfo(
+  //     appName: 'Unknown',
+  //     packageName: 'Unknown',
+  //     version: 'Unknown',
+  //     buildNumber: 'Unknown');
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _initPackageInfo();
+  // }
+  //
+  // Future<void> _initPackageInfo() async {
+  //   final PackageInfo info = await PackageInfo.fromPlatform();
+  //   setState(() {
+  //     _packageInfo = info;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Text(AppLocalizations.of(context)!.aboutPageAppDescription));
+        child: Column(
+      children: [
+        Image(image: AssetImage('assets/il_about.png')),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(AppLocalizations.of(context)!.appTitle),
+              // Text('Version ${_packageInfo.version} (build ${_packageInfo.buildNumber})'),
+              Text(AppLocalizations.of(context)!.aboutPageAppDescription),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
