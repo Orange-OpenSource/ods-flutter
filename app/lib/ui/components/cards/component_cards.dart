0f81419d980f49efa04fbaf6815f7183935964fb
Pauline Auvray <pauline.auvray@orange.com>
2023-04-25 17:10:36 +0200

    Fix card screens background color

--- app/lib/ui/components/cards/component_cards.dart
+++ app/lib/ui/components/cards/component_cards.dart
@@ -4,7 +4,7 @@ class ComponentCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         title: Text('Cards'),
       ),
