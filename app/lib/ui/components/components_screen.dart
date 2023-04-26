d439090c833b064263cea40543b593c4a90bbb64
Doline Touko <doline.touko@orange.com>
2023-04-26 18:03:01 +0200

    Upade with main

--- app/lib/ui/components/components_screen.dart
+++ app/lib/ui/components/components_screen.dart
@@ -1,68 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:ods_flutter/components/ods_localized_component.dart';
+import 'package:ods_flutter/components/card/ods_small_card.dart';
+import 'package:ods_flutter/guidelines/spacings.dart';
+
+import 'component.dart';
 
 class ComponentsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: ListView(
-        children: [
-          TextButton(
-            style: TextButton.styleFrom(
-              textStyle: const TextStyle(fontSize: 24),
-            ),
-            onPressed: () {},
-            child: Text('ODS components'),
-          ),
-          Card(
-            child: ListTile(title: OdsLocalizedComponent()),
-          ),
-          Card(
-            child: ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, '/component_cards');
-              },
-              title: const Center(child: Text('Cards')),
-            ),
-          ),
-          Card(
-              child: ListTile(
-            onTap: () {
-              Navigator.pushNamed(context, '/component_checkboxes');
-            },
-            title: const Center(child: Text('Checkboxes')),
-          )),
-          Card(
-              child: ListTile(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/component_radioButtons');
-                  },
-                  title: const Center(child: Text('Radio Buttons')))),
-          Card(
-            child: ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, '/component_switches');
-              },
-              title: const Center(child: Text('Switches')),
-            ),
-          ),
-          TextButton(
-            style: TextButton.styleFrom(
-              textStyle:
-                  const TextStyle(fontSize: 24, decorationColor: Colors.black),
-            ),
-            onPressed: () {},
-            child: Text('Material'),
-          ),
-          Card(
-              child: ListTile(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/component_material');
-                  },
-                  title: const Center(child: Text('See all')))),
-        ],
-      ),
-    ));
+            child: Padding(
+                padding: const EdgeInsets.only(
+                    left: spacingXs, right: spacingXs, top: spacingS),
+                child: GridView.count(
+                    crossAxisCount: 2,
+                    children:
+                        List.generate(group.getComponent().length, (index) {
+                      return OdsSmallCard(
+                        title: group.getName(index),
+                        image: Image.asset(group.getImage(index),
+                            semanticLabel: 'Flutter image',
+                            fit: BoxFit.fitHeight),
+                        onTap: () {
+                          Navigator.pushNamed(
+                              context, group.getDirections(index));
+                        },
+                      );
+                    })))));
   }
+
+  Components group = Components();
 }
