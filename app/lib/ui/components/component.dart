3d0e0745f91f9bfaedcd402a20caadc6e01b1597
Doline Touko <doline.touko@orange.com>
2023-05-16 11:18:16 +0200

    add changes

--- app/lib/ui/components/component.dart
+++ app/lib/ui/components/component.dart
@@ -108,4 +108,5 @@ class Components {
   }
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
 */
--- app/lib/ui/components/components_screen.dart
+++ app/lib/ui/components/components_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ods_flutter/components/card/ods_small_card.dart';
 import 'package:ods_flutter/guidelines/spacings.dart';
+import 'package:ods_flutter_demo/ui/components/utilities/display_image.dart';
 
-import '../utilities/display_image.dart';
 import 'component.dart';
 
 class ComponentsScreen extends StatefulWidget {
