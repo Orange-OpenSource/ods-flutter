8ccb6378bd63d939f81fc3304b6aa363e7481146
Doline Touko <doline.touko@orange.com>
2023-05-17 11:35:02 +0200

    Add comment to display image

--- app/lib/ui/utilities/display_image.dart
+++ app/lib/ui/utilities/display_image.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+///
+/// displayImage is used to return a specific widget based on image type
+/// * [imagePath] represent the path of the image
+/// * [width] represent the width of the image
 Widget displayImage(String imagePath, double? width) {
   if (imagePath.contains('svg')) {
     return SvgPicture.asset(imagePath,
