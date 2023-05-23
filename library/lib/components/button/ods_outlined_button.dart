90088ef8db34e91ab9b2420dd67b7fa48510b8e5
Doline Touko <doline.touko@orange.com>
2023-05-23 10:48:45 +0200

    Add files in library

---
+++ library/lib/components/button/ods_outlined_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+/// ODS Design Button.
+///
+///   OdsButton is used to toggle the on/off state of a single setting.
+///   It can be set to null to disable the switch.
+///  * [title] Text displayed in the button
+/// * [enabled], Controls the enabled state of the button. When `false`, this button will not
+//  be clickable
+/// * [iconButton], If `false`, no icon will be displayed.
+/// * [icon], Widget of the icon.
+class OdsOutlinedButton extends StatelessWidget {
+  final String title;
+  final bool enabled;
+  final bool iconButton;
+  final Widget icon;
+  final Function()? onPressed;
+
+  const OdsOutlinedButton(
+      {Key? key,
+      required this.title,
+      required this.enabled,
+      required this.iconButton,
+      required this.icon,
+      this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return iconButton
+        ? OutlinedButton(
+            onPressed: enabled ? null : onPressed, child: Text(title))
+        : OutlinedButton.icon(
+            onPressed: enabled ? null : onPressed,
+            icon: icon,
+            label: Text(title));
+  }
+}
