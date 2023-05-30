cf0cb5aa5519736b56e1f474332f39b3158b199e
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-05-30 12:48:01 +0200

    accessibility support and changelog added

--- app/lib/ui/components/checkboxes/component_checkboxes.dart
+++ app/lib/ui/components/checkboxes/component_checkboxes.dart
@@ -20,57 +20,89 @@ class _ComponentCheckboxesState extends State<ComponentCheckboxes> {
     return Scaffold(
       appBar:
           MainAppBar(AppLocalizations.of(context)!.componentCheckboxesTitle),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(25),
-            child: Semantics(
-              header: true,
-              child: Text(
-                AppLocalizations.of(context)!.checkboxesVariantTitleInformation,
-                style: Theme.of(context).textTheme.bodyLarge,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Semantics(
+                header: true,
+                child: Text(
+                  AppLocalizations.of(context)!
+                      .checkboxesVariantTitleInformation,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
               ),
             ),
-          ),
-          CheckboxListTile(
-              value: isChecked0,
-              title: Text('Chocolate'),
+            Semantics(
+              label: 'Chocolate Checkbox',
+              value: isChecked0 ? 'Checked' : 'Unchecked',
               enabled: isEnable,
-              onChanged: (bool? value) {
-                setState(() {
-                  isChecked0 = value!;
-                });
-              }),
-          CheckboxListTile(
-            value: isChecked2,
-            title: Text('Butter'),
-            enabled: isEnable,
-            onChanged: (value) {
-              setState(() {
-                isChecked2 = value!;
-              });
-            },
-          ),
-          CheckboxListTile(
-              title: Text('Sugar'),
-              value: true,
+              child: ExcludeSemantics(
+                child: CheckboxListTile(
+                  value: isChecked0,
+                  title: Text('Chocolate'),
+                  enabled: isEnable,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      isChecked0 = value!;
+                    });
+                  },
+                ),
+              ),
+            ),
+            Semantics(
+              label: 'Butter Checkbox',
+              value: isChecked2 ? 'Checked' : 'Unchecked',
+              enabled: isEnable,
+              child: ExcludeSemantics(
+                child: CheckboxListTile(
+                  value: isChecked2,
+                  title: Text('Butter'),
+                  enabled: isEnable,
+                  onChanged: (value) {
+                    setState(() {
+                      isChecked2 = value!;
+                    });
+                  },
+                ),
+              ),
+            ),
+            Semantics(
+              label: 'Sugar Checkbox',
+              value: 'Checked',
               enabled: false,
-              onChanged: (value) {
-                setState(() {
-                  isChecked2 = value!;
-                });
-              }),
-          CheckboxListTile(
-              title: Text('Eggs'),
-              value: false,
+              child: ExcludeSemantics(
+                child: CheckboxListTile(
+                    title: Text('Sugar'),
+                    value: true,
+                    enabled: false,
+                    onChanged: (value) {
+                      setState(() {
+                        isChecked2 = value!;
+                      });
+                    }),
+              ),
+            ),
+            Semantics(
+              label: 'Eggs Checkbox',
+              value: 'Unchecked',
               enabled: false,
-              onChanged: (value) {
-                setState(() {
-                  isChecked2 = value!;
-                });
-              }),
-        ],
+              child: ExcludeSemantics(
+                child: CheckboxListTile(
+                    title: Text('Eggs'),
+                    value: false,
+                    enabled: false,
+                    onChanged: (value) {
+                      setState(() {
+                        isChecked2 = value!;
+                      });
+                    }),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
