bfd71ca72063d7a24728a392e468dd40b945a79c
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-06-20 12:25:03 +0200

    added bar navigation component screens and doc

--- app/lib/ui/components/navigation_bar/navigation_bar.dart
+++ app/lib/ui/components/navigation_bar/navigation_bar.dart
@@ -8,6 +8,7 @@ import 'package:ods_flutter_demo/ui/components/navigation_bar/navigarion_bar_cus
 import 'package:ods_flutter_demo/ui/components/utilities/customization_bottom_sheet.dart';
 import 'package:ods_flutter_demo/ui/main_app_bar.dart';
 import 'package:ods_flutter_demo/ui/utilities/component_count_row.dart';
+import 'package:ods_flutter_demo/ui/utilities/expandable_text.dart';
 
 class ComponentNavigationBar extends StatefulWidget {
   const ComponentNavigationBar({Key? key}) : super(key: key);
@@ -66,39 +67,81 @@ class _NavBarDemoState extends State<_NavBarDemo> {
     final NavigationBarCustomizationState? customizationState =
         NavigationBarCustomization.of(context);
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(
-          height: 100,
-          child: IndexedStack(
-            index: selectedIndex,
-            children: [
-              Center(child: Text('Coffee Screen')),
-              Center(child: Text('Cooking Pot Screen')),
-              Center(child: Text('Ice Cream Screen')),
-              Center(child: Text('Restaurant Screen')),
-              Center(child: Text('Favorites Screen')),
-            ],
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.all(spacingM),
-          child: Align(
-            alignment: Alignment.center,
-            child: OdsNavigationBar(
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-              destinations: _destinations(context)
-                  .sublist(0, customizationState?.numberOfItems),
+    List<Widget> navigationDestinations =
+        _destinations(context).sublist(0, customizationState?.numberOfItems);
+
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.only(bottom: 100),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            /// Screens for each navigation destination
+            SizedBox(
+              height: 110,
+              child: IndexedStack(
+                index: selectedIndex,
+                children: [
+                  Center(child: Text('Coffee Screen')),
+                  Center(child: Text('Cooking Pot Screen')),
+                  Center(child: Text('Ice Cream Screen')),
+                  Center(child: Text('Restaurant Screen')),
+                  Center(child: Text('Favorites Screen')),
+                ],
+              ),
             ),
-          ),
+
+            /// Navigation Bar icon
+            Padding(
+              padding: EdgeInsets.all(spacingM),
+              child: Align(
+                alignment: Alignment.center,
+                child: OdsNavigationBar(
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (index) {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+                  destinations: navigationDestinations,
+                ),
+              ),
+            ),
+            SizedBox(height: spacingM),
+
+            /// Documention for svg icon
+            Text(
+              'Icon of the navigation bar',
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            SizedBox(height: spacingM),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'If icons are provided in SVG format the system does not apply right color on images if selected. So we need to provide icon: and selectedIcon: parameters with right colorFilter using theme like this:',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            SizedBox(height: spacingM),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: ExpandableTextWidget(
+                text: destinationsSvgCode,
+                maxLines: 50,
+              ),
+            ),
+            SizedBox(height: spacingM),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'For images provided in PNG, there is nothing to do (just provide icon: parameter), the system do it itself.',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 
@@ -154,15 +197,17 @@ class _NavBarDemoState extends State<_NavBarDemo> {
           selectedIcon: SvgPicture.asset("assets/recipes/ic_restaurant.svg",
               colorFilter: activeColorFilter),
           label: "Restaurant"),
-      NavigationDestination(
-          tooltip: '',
-          icon:
-              SvgPicture.asset("assets/ic_heart.svg", colorFilter: colorFilter),
-          selectedIcon: SvgPicture.asset("assets/ic_heart.svg",
-              colorFilter: activeColorFilter),
-          label: "Favorites"),
     ];
   }
+
+  String destinationsSvgCode = '''
+  var colorScheme = Theme.of(context).colorScheme;
+  var activeColorFilter = ColorFilter.mode(colorScheme.primary, BlendMode.srcIn);
+  var colorFilter = ColorFilter.mode(colorScheme.secondary, BlendMode.srcIn);
+''';
+  String destinationsIconCode = '''
+For images provided in PNG, there is nothing to do (just provide icon: parameter), the system do it itself.
+''';
 }
 
 // Bottom Sheet content
@@ -180,7 +225,7 @@ class _CustomizationContent extends StatelessWidget {
             maxCount: NavigationBarCustomizationState.maxNavigationItemCount,
             count: customizationState!.numberOfItems,
             onChanged: (value) {
-              customizationState!.numberOfItems = value;
+              customizationState.numberOfItems = value;
             })
       ],
     );
