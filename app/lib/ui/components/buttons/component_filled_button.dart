9a813be39376ec76a72f0fc7f48c1b7f02b5cc53
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-06-22 16:20:50 +0200

    customization gestion for ods filledbutton

--- app/lib/ui/components/buttons/component_filled_button.dart
+++ app/lib/ui/components/buttons/component_filled_button.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_gen/gen_l10n/ods_flutter_app_localizations.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:ods_flutter/components/button/ods_filled_button.dart';
 import 'package:ods_flutter_demo/ui/components/buttons/component_button_customization.dart';
-import 'package:ods_flutter_demo/ui/components/material/component_material.dart';
 import 'package:ods_flutter_demo/ui/components/utilities/customization_bottom_sheet.dart';
 import 'package:ods_flutter_demo/ui/main_app_bar.dart';
 
@@ -45,44 +46,22 @@ class _ComponentFilledButtonsState extends State<ComponentFilledButtons> {
 class _Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Semantics(
+    final ButtonCustomizationState? customizationState =
+        ButtonCustomization.of(context);
+
+    return Semantics(
       header: true,
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: const <Widget>[
-            ButtonsVariants(isDisabled: false),
-            ButtonsVariants(isDisabled: true),
-          ],
-        ),
-      ),
-    ));
-  }
-}
-
-class ButtonsVariants extends StatelessWidget {
-  final bool isDisabled;
-
-  const ButtonsVariants({super.key, required this.isDisabled});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 5.0),
-      child: IntrinsicWidth(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            componentSpacer,
-            FilledButton(
-              onPressed: () {
-                // Action à exécuter lorsque le bouton est pressé
-              },
-              child: Text("Filled"),
-            ),
-          ],
+      child: Padding(
+        padding:
+            const EdgeInsets.only(left: 15, top: 25, right: 15, bottom: 15.0),
+        child: OdsFilledButton(
+          title: 'BUTTON',
+          icon: customizationState?.hasIcon == true
+              ? SvgPicture.asset('assets/ic_about_info.svg',
+                  width: 24, height: 24)
+              : null,
+          onPressed: customizationState?.hasEnabled == true ? () {} : null,
+          fullScreenWidth: customizationState?.hasFullScreen ?? false,
         ),
       ),
     );
@@ -97,17 +76,27 @@ class _CustomizationContent extends StatelessWidget {
     return Column(
       children: [
         SwitchListTile(
-            value: customizationState?.hasFullScreen ?? true,
-            title: Text(AppLocalizations.of(context)!.componentCardClickable),
-            onChanged: (bool value) {
-              customizationState?.hasFullScreen = value;
-            }),
+          value: customizationState?.hasIcon ?? true,
+          title: Text(AppLocalizations.of(context)!.componentCustomizeIcon),
+          onChanged: (bool value) {
+            customizationState?.hasIcon = value;
+          },
+        ),
         SwitchListTile(
-            value: customizationState?.hasIcon ?? true,
-            title: Text(AppLocalizations.of(context)!.componentElementSubtitle),
-            onChanged: (bool value) {
-              customizationState?.hasIcon = value;
-            })
+          value: customizationState?.hasFullScreen ?? true,
+          title:
+              Text(AppLocalizations.of(context)!.componentCustomizeFullScreen),
+          onChanged: (bool value) {
+            customizationState?.hasFullScreen = value;
+          },
+        ),
+        SwitchListTile(
+          value: customizationState?.hasEnabled ?? true,
+          title: Text(AppLocalizations.of(context)!.componentCustomizeEnable),
+          onChanged: (bool value) {
+            customizationState?.hasEnabled = value;
+          },
+        ),
       ],
     );
   }
