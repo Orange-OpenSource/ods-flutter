8ac1d8ee5b6a739d2b539aa9ffbbcec2412dc6d7
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-06-22 16:47:32 +0200

    customization gestion for ods outlinedbutton

--- library/lib/components/button/ods_text_button.dart
+++ library/lib/components/button/ods_text_button.dart
@@ -2,35 +2,81 @@ import 'package:flutter/material.dart';
 
 /// ODS Design Button.
 ///
-///  * [title] Text displayed in the button.
-/// * [enabled], Controls the state of the button. When `false`, this button will not
-///  be clickable.
-/// * [iconButton], If `false`, no icon will be displayed.
-/// * [icon], Widget of the icon.
-/// * [onPressed], used to specified the action when the button is selected.
-class OdsTextButton extends StatelessWidget {
+/// Displays a customizable button with an optional icon and a title.
+/// The button can expand to full screen width if specified.
+class OdsTextButton extends StatefulWidget {
+  /// Creates an ODS Text Button.
+  ///
+  /// * [title] - Text displayed in the button.
+  /// * [fullScreenWidth] - Specifies whether the button should expand to full screen width.
+  /// * [icon] - Widget of the icon.
+  /// * [onPressed] - The action to be executed when the button is pressed.
+  const OdsTextButton({
+    Key? key,
+    required this.title,
+    required this.fullScreenWidth,
+    this.icon,
+    this.onPressed,
+  }) : super(key: key);
+
+  /// The button's title displayed inside the button.
   final String title;
-  final bool enabled;
-  final bool iconButton;
-  final Widget icon;
+
+  /// Specifies whether the button should expand to full screen width.
+  final bool fullScreenWidth;
+
+  /// The optional button's icon.
+  final Widget? icon;
+
+  /// The action to be executed when the button is pressed.
   final Function()? onPressed;
 
-  const OdsTextButton(
-      {Key? key,
-      required this.title,
-      required this.enabled,
-      required this.iconButton,
-      required this.icon,
-      this.onPressed})
-      : super(key: key);
+  @override
+  State<OdsTextButton> createState() => _OdsTextButtonState();
+}
 
+class _OdsTextButtonState extends State<OdsTextButton> {
   @override
   Widget build(BuildContext context) {
-    return iconButton
-        ? TextButton(onPressed: enabled ? null : onPressed, child: Text(title))
-        : TextButton.icon(
-            onPressed: enabled ? null : onPressed,
-            icon: icon,
-            label: Text(title));
+    if (widget.icon != null && widget.fullScreenWidth == false) {
+      return TextButton.icon(
+        onPressed: widget.onPressed,
+        icon: widget.onPressed != null ? widget.icon! : _colorFilter(),
+        label: Text(widget.title),
+      );
+    }
+
+    if (widget.icon != null && widget.fullScreenWidth == true) {
+      return SizedBox(
+        width: double.infinity,
+        child: TextButton.icon(
+          onPressed: widget.onPressed,
+          icon: widget.onPressed != null ? widget.icon! : _colorFilter(),
+          label: Text(widget.title),
+        ),
+      );
+    }
+
+    if (widget.fullScreenWidth == true) {
+      return SizedBox(
+        width: double.infinity,
+        child: TextButton(
+          onPressed: widget.onPressed,
+          child: Text(widget.title),
+        ),
+      );
+    }
+
+    return TextButton(
+      onPressed: widget.onPressed,
+      child: Text(widget.title),
+    );
+  }
+
+  Widget _colorFilter() {
+    return ColorFiltered(
+      colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+      child: widget.icon,
+    );
   }
 }
