8f4dfe4406b1beab1e370fd93722992a71a3edd7
Benoit SUZANNE <benoit.suzanne@orange.com>
2023-09-01 18:55:24 +0200

    210 update the typography tokens for flutter (#211)
    * I've updated the fontsize according to the M3 sizes on both OS - should confirm if the letter spacing and the line spacing also need to be updated
    
    * Correction to update the M3 line height and letter spacing. Used the same values for iOS.
    On the font weight, the android ones have been aligned to the M3 setup (To be confirmed).

--- library/lib/theme/ods_typography.dart
+++ library/lib/theme/ods_typography.dart
@@ -20,39 +20,41 @@ TextTheme androidTextTheme = const TextTheme(
   headlineLarge: TextStyle(
       debugLabel: 'Headline L',
       fontFamily: 'Roboto',
-      fontSize: 34,
-      fontWeight: FontWeight.w700,
-      height: 1.1,
-      letterSpacing: 0.25),
+      fontSize: 32,
+      fontWeight: FontWeight.w400,
+      height: 1.25,
+      letterSpacing: 0),
   // Headline M > no correspondence
   headlineMedium: TextStyle(
       debugLabel: 'Headline M',
       fontFamily: 'Roboto',
       fontSize: 28,
-      fontWeight: FontWeight.w700,
-      letterSpacing: 0.25),
+      fontWeight: FontWeight.w400,
+      height: 1.29,
+      letterSpacing: 0),
   // Headline S > Android M2: Headline 5
   headlineSmall: TextStyle(
       debugLabel: 'Headline S',
       fontFamily: 'Roboto',
       fontSize: 24,
-      height: 1.8,
-      fontWeight: FontWeight.w700),
+      fontWeight: FontWeight.w400,
+      height: 1.33,
+      letterSpacing: 0),
 
   // Title L > Android M2: Headline 6
   titleLarge: TextStyle(
       debugLabel: 'Title L',
       fontFamily: 'Roboto',
-      fontSize: 20,
-      fontWeight: FontWeight.w700,
-      height: 1.2,
-      letterSpacing: 0.15),
+      fontSize: 22,
+      fontWeight: FontWeight.w400,
+      height: 1.27,
+      letterSpacing: 0),
   // Title M > Android M2: Subtitle 1
   titleMedium: TextStyle(
       debugLabel: 'Title M',
       fontFamily: 'Roboto',
       fontSize: 16,
-      fontWeight: FontWeight.w700,
+      fontWeight: FontWeight.w500,
       height: 1.5,
       letterSpacing: 0.15),
   // Title S > Android M2: Subtitle 2
@@ -60,8 +62,8 @@ TextTheme androidTextTheme = const TextTheme(
       debugLabel: 'Title S',
       fontFamily: 'Roboto',
       fontSize: 14,
-      fontWeight: FontWeight.w700,
-      height: 1.7,
+      fontWeight: FontWeight.w500,
+      height: 1.5,
       letterSpacing: 0.1),
 
   // Body L > Android M2: Body 1
@@ -70,7 +72,7 @@ TextTheme androidTextTheme = const TextTheme(
       fontFamily: 'Roboto',
       fontSize: 16,
       fontWeight: FontWeight.w400,
-      height: 1.25,
+      height: 1.5,
       letterSpacing: 0.5),
   // Body M > Android M2: Body 2
   bodyMedium: TextStyle(
@@ -95,122 +97,143 @@ TextTheme androidTextTheme = const TextTheme(
       fontFamily: 'Roboto',
       fontSize: 14,
       fontWeight: FontWeight.w500,
-      height: 1.14,
-      letterSpacing: 1.25),
+      height: 1.43,
+      letterSpacing: 0.1),
   // Label M > Android M2: NA
   labelMedium: TextStyle(
       debugLabel: 'Label M',
       fontFamily: 'Roboto',
       fontSize: 12,
       fontWeight: FontWeight.w500,
-      height: 1.35,
-      letterSpacing: 1.25),
+      height: 1.33,
+      letterSpacing: 0.5),
   // Label S > Android M2: Overline
   labelSmall: TextStyle(
       debugLabel: 'Label S',
       fontFamily: 'Roboto',
-      fontSize: 10,
-      fontWeight: FontWeight.w400,
-      height: 1.6,
-      letterSpacing: 1.5),
+      fontSize: 11,
+      fontWeight: FontWeight.w500,
+      height: 1.45,
+      letterSpacing: 0.5),
 );
+
+/*
+M3 Reference in Flutter implementation
+    displayLarge: TextStyle(fontSize: 57.0, fontWeight: FontWeight.w400, letterSpacing: -0.25, height: 1.12),
+    displayMedium: TextStyle(fontSize: 45.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, height: 1.16),
+    displaySmall: TextStyle(fontSize: 36.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, height: 1.22),
+    headlineLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, height: 1.25),
+    headlineMedium: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, height: 1.29),
+    headlineSmall: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, height: 1.33),
+    titleLarge: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, height: 1.27),
+    titleMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500, letterSpacing: 0.15, height: 1.50),
+    titleSmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500, letterSpacing: 0.1, height: 1.43),
+    bodyLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, letterSpacing: 0.5, height: 1.50),
+    bodyMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, letterSpacing: 0.25, height: 1.43),
+    bodySmall: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, letterSpacing: 0.4, height: 1.33),
+    labelLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500, letterSpacing: 0.1, height: 1.43),
+    labelMedium: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w500, letterSpacing: 0.5, height: 1.33),
+    labelSmall: TextStyle(fontSize: 11.0, fontWeight: FontWeight.w500, letterSpacing: 0.5, height: 1.45),
+ */
 
 TextTheme iosTextTheme = const TextTheme(
   // Headline L > iOS: Large Title
   headlineLarge: TextStyle(
       debugLabel: 'Headline L',
       fontFamily: '.SF UI Display',
-      fontSize: 34,
+      fontSize: 32,
       fontWeight: FontWeight.w700,
-      height: 1.2,
-      letterSpacing: 0.37),
+      height: 1.25,
+      letterSpacing: 0),
   // Headline M > no correspondence
   headlineMedium: TextStyle(
       debugLabel: 'Headline M',
       fontFamily: '.SF UI Display',
       fontSize: 28,
       fontWeight: FontWeight.w700,
-      height: 1.3),
+      height: 1.29,
+      letterSpacing: 0),
   // Headline S > iOS: Headline
   headlineSmall: TextStyle(
       debugLabel: 'Headline S',
       fontFamily: '.SF UI Display',
-      fontSize: 17,
+      fontSize: 24,
       fontWeight: FontWeight.w700,
-      height: 1.3,
-      letterSpacing: -0.41),
+      height: 1.33,
+      letterSpacing: 0),
 
   // Title L > iOS: Title 1
   titleLarge: TextStyle(
       debugLabel: 'Title L',
       fontFamily: '.SF UI Display',
-      fontSize: 28,
+      fontSize: 22,
       fontWeight: FontWeight.w700,
-      height: 1.21,
-      letterSpacing: 0.36),
+      height: 1.27,
+      letterSpacing: 0),
   // Title M > iOS: Title 2
   titleMedium: TextStyle(
       debugLabel: 'Title M',
       fontFamily: '.SF UI Display',
-      fontSize: 22,
+      fontSize: 16,
       fontWeight: FontWeight.w700,
-      height: 1.27,
-      letterSpacing: 0.35),
+      height: 1.5,
+      letterSpacing: 0.15),
   // Title S > iOS: Title 3
   titleSmall: TextStyle(
       debugLabel: 'Title S',
       fontFamily: '.SF UI Display',
-      fontSize: 20,
+      fontSize: 14,
       fontWeight: FontWeight.w700,
-      height: 1.25,
-      letterSpacing: 0.38),
+      height: 1.43,
+      letterSpacing: 0.1),
 
   // Body L > iOS: Body
   bodyLarge: TextStyle(
       debugLabel: 'Body L',
       fontFamily: '.SF UI Display',
-      fontSize: 17,
+      fontSize: 16,
       fontWeight: FontWeight.w400,
-      height: 1.29,
-      letterSpacing: 0.41),
+      height: 1.5,
+      letterSpacing: 0.5),
   // Body M > iOS: Callout
   bodyMedium: TextStyle(
       debugLabel: 'Body M',
       fontFamily: '.SF UI Display',
-      fontSize: 16,
+      fontSize: 14,
       fontWeight: FontWeight.w400,
-      height: 1.31,
-      letterSpacing: 0.32),
+      height: 1.43,
+      letterSpacing: 0.25),
   // Body S > iOS: Subheadline
   bodySmall: TextStyle(
       debugLabel: 'Body S',
       fontFamily: '.SF UI Display',
-      fontSize: 15,
-      fontWeight: FontWeight.w700,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
       height: 1.33,
-      letterSpacing: -0.5),
+      letterSpacing: 0.4),
 
   // Label L > iOS: Footnote
   labelLarge: TextStyle(
       debugLabel: 'Label L',
       fontFamily: '.SF UI Display',
-      fontSize: 13,
-      fontWeight: FontWeight.w400,
-      height: 1.38,
-      letterSpacing: -0.08),
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      height: 1.43,
+      letterSpacing: 0.1),
   // Label M > iOS: Caption 1
   labelMedium: TextStyle(
       debugLabel: 'Label M',
       fontFamily: '.SF UI Display',
       fontSize: 12,
+      fontWeight: FontWeight.w500,
       height: 1.33,
-      fontWeight: FontWeight.w400),
+      letterSpacing: 0.5),
   // Label S > iOS: Caption 2
   labelSmall: TextStyle(
       debugLabel: 'Label S',
       fontFamily: '.SF UI Display',
       fontSize: 11,
-      fontWeight: FontWeight.w400,
-      height: 1.18,
-      letterSpacing: 0.08),
+      fontWeight: FontWeight.w500,
+      height: 1.45,
+      letterSpacing: 0.5),
 );
