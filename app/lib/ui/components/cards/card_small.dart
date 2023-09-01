3d74d15be711459e8b62ded98c3df9d482f388d3
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-09-01 11:08:17 +0200

    review : show only one item

--- app/lib/ui/components/cards/card_small.dart
+++ app/lib/ui/components/cards/card_small.dart
@@ -40,12 +40,13 @@ class _CardSmallState extends State<CardSmall> {
 }
 
 class _Body extends StatelessWidget {
+  var recipe =
+      OdsApplication.recipes[Random().nextInt(OdsApplication.recipes.length)];
+
   @override
   Widget build(BuildContext context) {
     final CardCustomizationState? customizationState =
         CardCustomization.of(context);
-    var recipes =
-        OdsApplication.recipes[Random().nextInt(OdsApplication.recipes.length)];
 
     return SafeArea(
       child: OrientationBuilder(
@@ -53,35 +54,30 @@ class _Body extends StatelessWidget {
           return GridView.count(
             crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
             childAspectRatio: orientation == Orientation.portrait ? 0.9 : 1.1,
-            children: OdsApplication.recipes
-                .map(
-                  (recipe) {
-                    return Column(
-                      children: [
-                        OdsSmallCard(
-                          title: recipes.title,
-                          subtitle: customizationState?.hasSubtitle == true
-                              ? recipes.subtitle
-                              : null,
-                          image: FadeInImage(
-                            placeholder: AssetImage('assets/placeholder.png'),
-                            image: NetworkImage(recipes.url),
-                            fit: BoxFit.cover,
-                            imageErrorBuilder: (context, error, stackTrace) {
-                              return Image(
-                                image: AssetImage('assets/placeholder.png'),
-                                fit: BoxFit.cover,
-                              );
-                            },
-                          ),
-                          onTap: customizationState!.clickable ? () {} : null,
-                        ),
-                      ],
-                    );
-                  },
-                )
-                .take(1) // Take only the first element from the map
-                .toList(),
+            children: [
+              Column(
+                children: [
+                  OdsSmallCard(
+                    title: recipe.title,
+                    subtitle: customizationState?.hasSubtitle == true
+                        ? recipe.subtitle
+                        : null,
+                    image: FadeInImage(
+                      placeholder: AssetImage('assets/placeholder.png'),
+                      image: NetworkImage(recipe.url),
+                      fit: BoxFit.cover,
+                      imageErrorBuilder: (context, error, stackTrace) {
+                        return Image(
+                          image: AssetImage('assets/placeholder.png'),
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ),
+                    onTap: customizationState!.clickable ? () {} : null,
+                  ),
+                ],
+              ),
+            ],
           );
         },
       ),
