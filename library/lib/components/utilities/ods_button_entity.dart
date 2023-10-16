afcb3df59afa8cd91c964aaa410d7538cac40c83
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-10-16 12:41:51 +0200

    lib : change documentation in code

--- library/lib/components/utilities/ods_button_entity.dart
+++ library/lib/components/utilities/ods_button_entity.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
 
 /// Button entity used in ODS components.
-/// It represents a simple button with a [label] and an associated action executed [onClick].
+/// It represents a simple button with a [label]
+/// Will be called when the user clicks the button [onClick].
 class OdsButtonEntity {
   const OdsButtonEntity({
     required this.label,
