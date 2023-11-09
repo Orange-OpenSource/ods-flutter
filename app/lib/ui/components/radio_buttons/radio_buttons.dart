9a77a2caae2bf90abbcb44319b67a1e5cf77b09f
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-11-09 19:12:39 +0100

    app : add radio buttons component

--- app/lib/ui/components/radio_buttons/radio_buttons.dart
+++ app/lib/ui/components/radio_buttons/radio_buttons.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/ods_flutter_app_localizations.dart';
 import 'package:ods_flutter/components/sheets_bottom/ods_sheets_bottom.dart';
-import 'package:ods_flutter_demo/main.dart';
 import 'package:ods_flutter_demo/ui/components/radio_buttons/radio_buttons_customization.dart';
 import 'package:ods_flutter_demo/ui/main_app_bar.dart';
 
@@ -12,7 +11,7 @@ class ComponentRadioButtons extends StatefulWidget {
   State<ComponentRadioButtons> createState() => _ComponentRadioButtonsState();
 }
 
-enum Options { option1, option2, option3 }
+enum Options { option1, option2 }
 
 class _ComponentRadioButtonsState extends State<ComponentRadioButtons> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -52,55 +51,36 @@ class __BodyState extends State<_Body> {
     final RadioButtonsCustomizationState? customizationState =
         RadioButtonsCustomization.of(context);
 
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            RadioListTile<Options>(
-              title: Text(OdsApplication.recipes[0].title),
-              value: Options.option1,
-              groupValue: _selectedOption,
-              onChanged: customizationState?.hasEnabled == true
-                  ? (value) {
-                      setState(
-                        () {
-                          _selectedOption = value;
-                        },
-                      );
-                    }
-                  : null,
-            ),
-            RadioListTile<Options>(
-              title: Text(OdsApplication.recipes[1].title),
-              value: Options.option2,
-              groupValue: _selectedOption,
-              onChanged: customizationState?.hasEnabled == true
-                  ? (value) {
-                      setState(
-                        () {
-                          _selectedOption = value;
-                        },
-                      );
-                    }
-                  : null,
-            ),
-            RadioListTile<Options>(
-              title: Text(OdsApplication.recipes[2].title),
-              value: Options.option3,
-              groupValue: _selectedOption,
-              onChanged: customizationState?.hasEnabled == true
-                  ? (value) {
-                      setState(
-                        () {
-                          _selectedOption = value;
-                        },
-                      );
-                    }
-                  : null,
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Radio(
+            value: Options.option1,
+            groupValue: _selectedOption,
+            onChanged: customizationState?.hasEnabled == true
+                ? (Options? value) {
+                    setState(
+                      () {
+                        _selectedOption = value;
+                      },
+                    );
+                  }
+                : null,
+          ),
+          Radio(
+            value: Options.option2,
+            groupValue: _selectedOption,
+            onChanged: customizationState?.hasEnabled == true
+                ? (Options? value) {
+                    setState(
+                      () {
+                        _selectedOption = value;
+                      },
+                    );
+                  }
+                : null,
+          ),
+        ],
       ),
     );
   }
