6ddb21c7b1187b02ed6b229d04768352282dc13a
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-11-09 19:59:13 +0100

    library : add ods list radio button

---
+++ library/lib/components/lists/ods_list_radio_button.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:ods_flutter/l10n/gen/ods_localizations.dart';
+
+/// ODS OdsListRadioButton.
+///
+/// The OdsCheckbox widget represents a checkbox element that can be checked or unchecked.
+/// It allows for handling the checkbox state and triggering a callback when its state changes.
+class OdsListRadioButton<T> extends StatefulWidget {
+  /// Creates an ODS OdsListRadioButton.
+  const OdsListRadioButton({
+    Key? key,
+    required this.text,
+    required this.value,
+    required this.groupValue,
+    required this.onCheckedChange,
+    this.enabled = true,
+  }) : super(key: key);
+
+  /// The primary content of the list tile
+  final String text;
+
+  /// The value.
+  final T value;
+
+  /// The group value
+  final T? groupValue;
+
+  /// A callback function to handle changes in the checked state.
+  final void Function(T?)? onCheckedChange;
+
+  /// Controls the enabled state of the radio button. When false, this button will not be clickable.
+  final bool? enabled;
+
+  @override
+  State<OdsListRadioButton<T>> createState() => _OdsRadioButtonState<T>();
+}
+
+class _OdsRadioButtonState<T> extends State<OdsListRadioButton<T>> {
+  @override
+  Widget build(BuildContext context) {
+    /*
+    String radioButtonValue = widget.value == null
+        ? OdsLocalizations.of(context)!.componentCheckboxesIndeterminate
+        : widget.value ?? false
+        ? OdsLocalizations.of(context)!.componentCheckboxesChecked
+        : OdsLocalizations.of(context)!.componentCheckboxesUnchecked;
+
+     */
+
+    return Semantics(
+      //value: radioButtonValue,
+      enabled: widget.enabled,
+      hint: OdsLocalizations.of(context)!.componentCheckboxes,
+      excludeSemantics: true,
+      child: RadioListTile<T>(
+        title: Text(widget.text),
+        value: widget.value,
+        onChanged: widget.onCheckedChange,
+        groupValue: widget.groupValue,
+      ),
+    );
+  }
+}
