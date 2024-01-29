23196508c66159376ec815efc0e08c6100bb3d37
Tayeb Sedraia <tayeb.sedraia@orange.com>
2024-01-29 12:36:35 +0100

    app : delete component textfield from material example

--- app/lib/ui/components/material/component_material.dart
+++ app/lib/ui/components/material/component_material.dart
@@ -31,7 +31,7 @@ class _ComponentMaterialState extends State<ComponentMaterial> {
           //ProgressIndicators(),
           //Sliders(),
           //SnackBars(),
-          TextFields(),
+          //TextFields(),
         ],
       ),
     );
