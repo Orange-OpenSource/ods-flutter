ebc2b868d59c4e28ed8120d2bc37eeeb3b217fe5
Tayeb Sedraia <tayeb.sedraia@orange.com>
2024-02-08 19:39:41 +0100

    app : add leader in source file

--- app/lib/ui/components/component_entities.dart
+++ app/lib/ui/components/component_entities.dart
@@ -1,3 +1,15 @@
+/*
+ * Software Name : Orange Design System
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT licence,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
+
 import 'dart:core';
 
 import 'package:flutter/material.dart';
@@ -8,7 +20,8 @@ class Component {
   String description;
   List<Variant> variants;
 
-  Component(this.title, this.imageResourceName, this.description, this.variants);
+  Component(
+      this.title, this.imageResourceName, this.description, this.variants);
 }
 
 class Variant {
