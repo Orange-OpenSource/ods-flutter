16d69aaa1b1587bb4ac7f70163a10935a8754969
Tayeb Sedraia <tayeb.sedraia@orange.com>
2024-02-09 10:46:55 +0100

    303 update copyright in files (#317)
    * update license file
    
    * app : add leader in source file
    
    * app and library : add changelog
    
    * library : add leader in source file

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,15 @@
+/*
+ * Software Name : Orange Design System
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT licence,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
+
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
--- app/lib/ui/components/lists/lists_checkboxes.dart
+++ app/lib/ui/components/lists/lists_checkboxes.dart
@@ -1,3 +1,15 @@
+/*
+ * Software Name : Orange Design System
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT licence,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/ods_flutter_app_localizations.dart';
 import 'package:ods_flutter/components/lists/ods_list_checkbox.dart';
