30bc50da9ce2f5b262b2cc2bbbedc05198cd1245
Tayeb Sedraia <tayeb.sedraia@orange.com>
2024-10-01 15:08:57 +0200

    Message in the README and test fixes to archive the project (#387)
    * fix : using webwiew library version 3.0.4
    
    * added warning message in readme

--- README.md
+++ README.md
@@ -43,6 +43,10 @@ Have a bug or a feature request? Please first search for existing and closed iss
 
 Please read through our [contributing guidelines](https://github.com/Orange-OpenSource/ods-flutter/blob/main/CONTRIBUTING.md). Included are directions for opening issues, coding standards, and notes on development.
 
+## Project Temporarily Archived
+
+This project has been archived and is no longer actively maintained. No further updates, bug fixes, or support will be provided. For the time being, this project will remain in its archived state. While it may still offer value, we recommend exploring other solutions or alternatives for ongoing support and updates
+
 ## Copyright and license
 
 Code released under the [MIT License](https://github.com/Orange-OpenSource/ods-flutter/blob/main/LICENSE).
--- app/lib/ui/about/detail/about_file_screen.dart
+++ app/lib/ui/about/detail/about_file_screen.dart
@@ -10,6 +10,7 @@
  * Software description: Flutter library of reusable graphical components for Android and iOS
  */
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -18,6 +19,7 @@ import 'package:flutter/services.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:ods_flutter/components/app_bar/top/ods_top_app_bar.dart';
 import 'package:path/path.dart' as path;
+import 'package:webview_flutter/webview_flutter.dart';
 
 class OdsAboutFileScreen extends StatelessWidget {
   final String title;
@@ -40,7 +42,6 @@ class OdsAboutFileScreen extends StatelessWidget {
         title: title,
         navigationIcon: BackButton(),
       ),
-      /*
       body: SafeArea(
         child: FutureBuilder(
           future: _loadFileData(),
@@ -74,8 +75,6 @@ class OdsAboutFileScreen extends StatelessWidget {
           },
         ),
       ),
-
-       */
     );
   }
 
